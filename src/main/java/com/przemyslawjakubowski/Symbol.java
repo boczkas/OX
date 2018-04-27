71e41374d9c683a79ba4087c4617a7827d0def1b
boczkas <przemyslawjakubowski@gmail.com>
2018-04-27 11:30:27 +0200

    Dodano kolorki

--- src/main/java/com/przemyslawjakubowski/Symbol.java
+++ src/main/java/com/przemyslawjakubowski/Symbol.java
@@ -1,5 +1,17 @@
 package com.przemyslawjakubowski;
 
 public enum Symbol {
-    X, O
+    X(Color.ANSI_CYAN + "X" + Color.ANSI_DEFAULT),
+    O(Color.ANSI_YELLOW + "O" + Color.ANSI_DEFAULT);
+
+    private String stringRepresentation;
+
+    Symbol(String stringRepresentation){
+        this.stringRepresentation = stringRepresentation;
+    }
+
+    @Override
+    public String toString(){
+        return stringRepresentation;
+    }
 }
