3d7acd92cf5d6830a6a89804f669e9e2e495c474
boczkas <przemyslawjakubowski@gmail.com>
2018-05-08 16:38:14 +0200

    Tests: more tests added

--- src/test/java/gameConfiguration/SymbolsToWinConfigurationStateTest.java
+++ src/test/java/gameConfiguration/SymbolsToWinConfigurationStateTest.java
@@ -11,6 +11,8 @@ import com.przemyslawjakubowski.textOutput.LanguageStrings;
 import com.przemyslawjakubowski.textOutput.OutputConsumer;
 import com.przemyslawjakubowski.userInput.UserInputProvider;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayInputStream;
@@ -19,23 +21,33 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class SymbolsToWinConfigurationStateTest {
+    String symbolsEntry;
+    OutputConsumer outputConsumer;
+    UserInputProvider userInputProvider;
+    ByteArrayOutputStream outContent;
+    LanguageStrings languageStrings;
+    XOGame game;
+
+    @BeforeMethod
+    public void setUp(){
+        symbolsEntry = "3\n";
+        outputConsumer = new OutputConsumer(System.out::println);
+        System.setIn(new ByteArrayInputStream(symbolsEntry.getBytes()));
+        userInputProvider = new UserInputProvider(new Scanner(System.in)::nextLine);
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        outputConsumer = new OutputConsumer(System.out::println);
+        languageStrings = LanguageFileReader.getLanguageStringsFromFile("ENG.lang", outputConsumer);
+        outputConsumer = new OutputConsumer(System.out::println, languageStrings);
+        game = new XOGame(userInputProvider, outputConsumer);
+    }
 
     @Test
     public void goToNextState_symbolsEntryCorrect_returns_StartingPlayerConfigurationState(){
         // given
-        String symbolsEntry = "3\n";
 
-        System.setIn(new ByteArrayInputStream(symbolsEntry.getBytes()));
-
-        UserInputProvider userInputProvider = new UserInputProvider(new Scanner(System.in)::nextLine);
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        OutputConsumer outputConsumer = new OutputConsumer(System.out::println);
-        LanguageStrings languageStrings = LanguageFileReader.getLanguageStringsFromFile("ENG.lang", outputConsumer);
-        outputConsumer = new OutputConsumer(System.out::println, languageStrings);
 
         // when
-        XOGame game = new XOGame(userInputProvider, outputConsumer);
         GameState symbolsToWinConfigurationState = new SymbolsToWinConfigurationState();
         game.getBoardStatus().setBoardColumns(3, outputConsumer);
         game.getBoardStatus().setBoardRows(3, outputConsumer);
@@ -47,19 +59,8 @@ public class SymbolsToWinConfigurationStateTest {
 
     @Test
     public void goToNextState_symbolsEntryIncorrect_returns_SymbolsToWinConfigurationState(){
-        String symbolsEntry = "3\n";
-
-        System.setIn(new ByteArrayInputStream(symbolsEntry.getBytes()));
-
-        UserInputProvider userInputProvider = new UserInputProvider(new Scanner(System.in)::nextLine);
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        OutputConsumer outputConsumer = new OutputConsumer(System.out::println);
-        LanguageStrings languageStrings = LanguageFileReader.getLanguageStringsFromFile("ENG.lang", outputConsumer);
-        outputConsumer = new OutputConsumer(System.out::println, languageStrings);
 
         // when
-        XOGame game = new XOGame(userInputProvider, outputConsumer);
         GameState symbolsToWinConfigurationState = new SymbolsToWinConfigurationState();
         game.getBoardStatus().setBoardColumns(2, outputConsumer);
         game.getBoardStatus().setBoardRows(2, outputConsumer);
