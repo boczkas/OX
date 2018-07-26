0c0ad454f50783a0fed61d450152ae24340bb8ed
Przemek <przemyslawjakubowski@gmail.com>
2018-07-26 15:02:27 +0200

    cos

--- src/main/java/com/przemyslawjakubowski/Main.java
+++ src/main/java/com/przemyslawjakubowski/Main.java
@@ -10,18 +10,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        UserInputProvider userInputProviderProvider = new UserInputProvider(new Scanner(System.in)::nextLine);
-        OutputConsumer outputConsumer = new OutputConsumer(System.out::println);
-        LanguageStrings languageStrings;
-
-        if(args.length > 0){
-            languageStrings = LanguageFileReader.getLanguageStringsFromFile(args[0], outputConsumer);
-        }
-        else {
-            languageStrings = LanguageFileReader.getDefaultLanguageStrings(outputConsumer);
-        }
-
-        outputConsumer = new OutputConsumer(System.out::println, languageStrings);
-        new XOGame(userInputProviderProvider, outputConsumer).start();
     }
 }
